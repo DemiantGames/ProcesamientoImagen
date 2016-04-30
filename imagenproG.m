021eb718603c4eea29322ed04571dcefc747c97f
Demián <demiandal@gmail.com>
2016-04-30 14:52:03 -0500

    Act 2

--- imagenproG.m
+++ imagenproG.m
@@ -1,10 +1,14 @@
 video=videoinput('macvideo',1,'YCbCr422_1280x720');
 Fuente=getselectedsource(video);
-preview(video)
+c=1;
+%preview(video)
+while c==1
 imaggen=getsnapshot(video);
 s=ycbcr2rgb(imaggen);
 gris=rgb2gray(s);
+binari=im2bw(gris,0.5);
 figure(1)
 imshow(s)
 figure(2)
-imshow(gris)
+imshow(binari)
+end
